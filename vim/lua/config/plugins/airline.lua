8d83de5801c3cd482f23df725e23cda755395cec
Lingchu Xi <xilingchu@163.com>
2024-07-06 18:26:38 +0800

    Add the buffer tabes.

--- vim/lua/config/plugins/airline.lua
+++ vim/lua/config/plugins/airline.lua
@@ -5,9 +5,13 @@ return {
     	lazy = false, -- make sure we load this during startup if it is your main colorscheme
     	priority = 1000, -- make sure to load this before all the other start plugins
     	dependencies = "vim-airline/vim-airline-themes",
-    	config = function()
+    	init = function()
     		-- load the colorscheme here
-    		vim.g.airline_theme='papercolor'
+		vim.cmd([[
+    		let g:airline_theme='sol'
+		let g:airline#extensions#tabline#enabled = 1
+		let g:airline#extensions#tabline#formatter = 'unique_tail_improved'
+		]])
     	end,
   	}
 }
