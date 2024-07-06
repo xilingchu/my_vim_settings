ef65c843eabe8f68d42e209c30f2b5ccffd7f6e5
Lingchu Xi <xilingchu@163.com>
2024-07-06 15:01:52 +0800

    Add the plugin surround.

--- vim/lua/config/plugins.lua
+++ vim/lua/config/plugins.lua
@@ -1,8 +1,8 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -10,4 +10,5 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	-- require("config.plugins.tokyonight"),
 	require("config.plugins.airline"),
+	require("config.plugins.surround"),
 })
