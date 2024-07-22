8aa75e561a3da8f1a81c835e31d277a22c89f283
Lingchu Xi <xilingchu@163.com>
2024-07-22 11:28:22 +0800

    Change the lua settings.

--- vim/lua/config/lsp/lua.lua
+++ vim/lua/config/lsp/lua.lua
@@ -2,14 +2,21 @@ return {
 	settings = {
 		Lua = {
 			diagnostics = {
-				globals = {'vim',},
+				globals = {"vim", "lsqlite3"},
 			},
 			workspace = {
 				checkThirdParty = false,
+				library = {
+					vim.fn.expand'~/.luarocks/share/lua/5.1',
+					'/usr/share/lua/5.1',
+				}
 			},
 			completion = {
 				callSnippet = "Replace"
 			},
+			runtime = {
+				version = "Lua 5.1"
+			}
 		}
 	}
 }
