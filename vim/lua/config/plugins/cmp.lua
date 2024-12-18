7fb581ffa80b9c4f31aacf42c430ca06827a3091
Lingchu Xi <xilingchu@163.com>
2024-12-19 02:57:16 +0800

    Add the border of cmp.

--- vim/lua/config/plugins/cmp.lua
+++ vim/lua/config/plugins/cmp.lua
@@ -33,19 +33,26 @@ return {
 					-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
 					-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
 					vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-					vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+					-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
 				end,
 			},
 			window = {
-				-- completion = cmp.config.window.bordered(),
-				-- documentation = cmp.config.window.bordered(),
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
 			},
 			mapping = cmp.mapping.preset.insert({
 				['<C-b>'] = cmp.mapping.scroll_docs(-4),
 				['<C-f>'] = cmp.mapping.scroll_docs(4),
 				['<C-Space>'] = cmp.mapping.complete(),
 				['<C-e>'] = cmp.mapping.abort(),
-				['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				-- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				["<CR>"] = cmp.mapping(function(fallback)
+      						if cmp.get_active_entry() then
+      						  cmp.confirm({select = true})
+      						else
+      						  fallback()
+      						end
+    					end,{"i","c"}),
 				["<Tab>"] = cmp.mapping(function(fallback)
             				cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
           			end,
