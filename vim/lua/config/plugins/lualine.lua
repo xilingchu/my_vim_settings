3e1b07c2f81f0aeef4659a0aed7f0013cc5803de
Lingchu Xi <xilingchu@163.com>
2024-12-19 03:08:27 +0800

    Forget to add lualine

---
+++ vim/lua/config/plugins/lualine.lua
@@ -0,0 +1,14 @@
+return {
+	-- the colorscheme should be available when starting nvim
+	{
+	'nvim-lualine/lualine.nvim',
+    	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    	priority = 1000, -- make sure to load this before all the other start plugins
+    	dependencies = "nvim-tree/nvim-web-devicons",
+	opts = {
+			options = {
+				theme = 'material'
+			},
+		},
+	}
+}
