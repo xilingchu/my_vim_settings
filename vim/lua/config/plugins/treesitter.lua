69666fe4180478149b81a0fef3b819b2ed02a38c
Lingchu Xi <xilingchu@163.com>
2024-12-19 02:59:09 +0800

    Fix: the configuration of treesitter.

--- vim/lua/config/plugins/treesitter.lua
+++ vim/lua/config/plugins/treesitter.lua
@@ -1,26 +1,30 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	event = {"BufNewFile", "BufReadPre"},
-	opts = {
-		ensure_installed = {
-			"latex",
-			"c",
-			"lua",
-			"python",
-			"bash",
-			"fortran",
-			"vim",
-			"vimdoc",
-			"yaml",
-			"markdown",
-			"markdown_inline",
-		},
-		sync_install = true,
-		highlight = {
-			enable = true,
-		},
-		indent = {
-			enable = true,
-		},
-	}
+	-- event = {"BufNewFile", "BufReadPre"},
+	dependencies = {"neovim/nvim-lspconfig"},
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup{
+			ensure_installed = {
+				-- "latex",
+				"c",
+				"lua",
+				"python",
+				"bash",
+				"fortran",
+				"vim",
+				"vimdoc",
+				"yaml",
+				"markdown",
+				"markdown_inline",
+			},
+			sync_install = true,
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+		}
+	end
 }
