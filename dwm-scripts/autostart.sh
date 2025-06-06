67d7790500e8beccef85ceed9734258d2402a865
xilingchu <xilingchu@163.com>
2025-06-06 16:08:40 +0800

    update the autostart.

--- dwm-scripts/autostart.sh
+++ dwm-scripts/autostart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # xmodmap ~/.my_hkkb_keymap &
-# feh --recursive --randomize --bg-fill ~/.dwm/wallpaper.jpg &
+feh --recursive --randomize --bg-fill ~/.local/share/dwm/wallpaper.png &
 picom -b &
 bash ~/.local/share/dwm/tap-to-click.sh  &
 bash ~/.local/share/dwm/inverse-scroll.sh &
 bash ~/.local/share/dwm/statusbar_refresh.sh &
-fcitx &
+fcitx5 &
 
