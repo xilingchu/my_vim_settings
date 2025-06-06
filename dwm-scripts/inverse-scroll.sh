eb22f1c6501f51557941a1d0155c013c967dcd27
xilingchu <xilingchu@163.com>
2025-06-06 16:07:44 +0800

    Lenovo update for touchpad.

--- dwm-scripts/inverse-scroll.sh
+++ dwm-scripts/inverse-scroll.sh
@@ -2,7 +2,7 @@
 
 # Get id of touchpad and the id of the field corresponding to
 # natural scrolling
-id=`xinput list | grep "TouchPad" | cut -d'=' -f2 | cut -d'[' -f1`
+id=`xinput list | grep "Touchpad" | cut -d'=' -f2 | cut -d'[' -f1`
 natural_scrolling_id=`xinput list-props $id | \
                       grep "Natural Scrolling Enabled (" \
                       | cut -d'(' -f2 | cut -d')' -f1`
--- dwm-scripts/tap-to-click.sh
+++ dwm-scripts/tap-to-click.sh
@@ -2,7 +2,7 @@
 
 # Get id of touchpad and the id of the field corresponding to
 # tapping to click
-id=`xinput list | grep "TouchPad" | cut -d'=' -f2 | cut -d'[' -f1`
+id=`xinput list | grep "Touchpad" | cut -d'=' -f2 | cut -d'[' -f1`
 tap_to_click_id=`xinput list-props $id | \
                       grep "Tapping Enabled (" \
                       | cut -d'(' -f2 | cut -d')' -f1`
