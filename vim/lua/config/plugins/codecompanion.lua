243129516b7ca1cac853b70b7d2499950c53e230
xilingchu <xilingchu@163.com>
2025-06-11 19:14:13 +0800

    Add a new model.

--- vim/lua/config/plugins/codecompanion.lua
+++ vim/lua/config/plugins/codecompanion.lua
@@ -59,6 +59,20 @@ return {
 						model = {default = model},
 					},
 				})
+				end,
+			my_claude = function()
+				return require("codecompanion.adapters").extend("openai_compatible", {
+        			env = {
+        			  url = "https://api.gpt.ge", -- optional: default value is ollama url http://127.0.0.1:11434
+        			  api_key = os.getenv("OPENAI_API_KEY"), -- optional: if your endpoint is authenticated
+        			  chat_url = "/v1/chat/completions", -- optional: default value, override if different
+        			  models_endpoint = "/v1/models", -- optional: attaches to the end of the URL to form the endpoint to retrieve models
+        			},
+        			schema = {
+        			  model = {
+        			    default = "gpt-4",  -- define llm model to be used
+        			  },
+				}})
 				end
 			},
 		-- Strategies for different interaction modes
@@ -66,7 +80,7 @@ return {
 		-- inline: Inline suggestions with accept/reject options
     		strategies = {
 			chat = {
-				adapter = "deepseek",
+				adapter = "my_claude",
 				keymaps = {
         				send = {
         				  modes = { n = "<C-s>", i = "<C-s>" },
@@ -77,7 +91,7 @@ return {
 				},
 			},
 			inline = {
-				adapter = "deepseek",
+				adapter = "my_claude",
 				keymaps = {
         				accept_change = {
         				  modes = { n = "ga" },
