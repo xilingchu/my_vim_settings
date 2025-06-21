a9d734da62d03a5196545d9a00b767ad01ecbfb6
xilingchu <xilingchu@163.com>
2025-06-21 21:28:17 +0800

    Remove completeopt

--- vim/lua/config/plugins/cmp.lua
+++ vim/lua/config/plugins/cmp.lua
@@ -28,9 +28,9 @@ return {
 		local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
 		cmp.setup({
 			-- For Rust
-			completion = {
-				completeopt = vim.o.completeopt
-			},
+			-- completion = {
+			-- 	completeopt = vim.o.completeopt
+			-- },
 			snippet = {
 				expand = function(args)
 					-- REQUIRED - you must specify a snippet engine expand = function(args)
